5a1d7f449eafc6f0e774e039a4f47e20dd847e02
Jonatas Emanuel <jonatassilvaemanuel@gmail.com>
2022-07-04 21:07:31 -0300

    quicksort

--- tes.py
+++ tes.py
@@ -1,40 +1,55 @@
-def som(arr):
-    if not arr:
-        return 0
-    else:
-        return arr[0] + som(arr[1:])
-
-
-def maior(arr):
-
-    if len(arr) == 2:
-        if arr[0] > arr[1]:
-            return arr[0]
+def binary_search(arr, item, left=0, right=None):
+    if right is None:
+        right = len(arr) - 1
+    if left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == item:
+            return mid
+        if item < arr[mid]:
+            return binary_search(arr, item, left, mid-1)
         else:
-            return arr[1]
-    
-    sub = maior(arr[1:])
-    if arr[0] > sub:
-        return arr[0]
-    else:
-        return sub
+            return binary_search(arr, item, mid+1, right)
+    return None
 
 
-def count(arr):
-    if not arr:
-        return 0
-    else:
-        return 1 + count(arr[1:])
+def selection_sort(arr):
+    for item in range(len(arr) - 1):
+        min_index = item
+        for min in range(item, len(arr)):
+            if arr[min] < arr[min_index]:
+                min_index = min
+ 
+        if arr[item] > arr[min_index]:
+            aux = arr[item]
+            arr[item] = arr[min_index]
+            arr[min_index] = aux
+
+    return arr
 
 
-def fat(num):
-    if num == 1:
-        return 1
-    else:
-        return num * fat(num - 1)
+def quicksort(arr, left=0, right=None):
+    if right is None:
+        right = len(arr) - 1
+    if left < right:
+        p = partition(arr, left, right)
+        quicksort(arr, left, p-1)
+        quicksort(arr, p+1, right)
+    return arr
+
+def partition(arr, left, right):
+    pivot = arr[right]
+    i = left
+    for j in range(left, right):
+        if arr[j] <= pivot:
+            arr[j], arr[i] = arr[i], arr[j]
+            i += 1
+    arr[i], arr[right] = arr[right], arr[i]
+    return i
 
 
-# print(som([2,3,5]))
-# print(maior([78, 5, 1, 340]))
-print(count([2, 45, 4, 6, 34, 3]))
-print(fat(5))
+lis = [999, 32, 764, 3, 232, 6, 65, 734, 1]
+#  quicksort(lis)
+#  print(lis)
+#  print(binary_search(lis, 764))
+selection_sort(lis)
+print(lis)
